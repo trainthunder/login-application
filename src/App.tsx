382fda01b264241208f426a4fa73817bcd4625ef
trainthunder <kitikun.po@gmail.com>
2024-09-20 23:28:41 +0700

    fix: delete import react

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/login-page";
 
--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import LoginForm from "../components/login-page/login-form";
 
 type Props = {};
